1f48aa7e12af810191875bc1993dcd577c7922fd
himani685 <140187176+himani685@users.noreply.github.com>
2024-01-28 19:08:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function Dahej(){
+    alert("kutte teri bhn ko naman");
+    let dharpakad = document.getElementById("d101").innerText
+ // console.log(dharpakad)
+ dharpakad  =  "dein beton ko vardan, krke dahej s inkar"
+ document.getElementById("d101").append(dharpakad)
+    
+  }
+  // function double(){
+  //   alert("baba ji ka thullu");
+  // }
+  
